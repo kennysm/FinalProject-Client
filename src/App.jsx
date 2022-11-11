38add30f8795484b1a9e5b0e0446ef39d1f5ca7e
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-11-10 21:39:04 -0300

    homescreen created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import "./App.scss";
+import HomeScreen from "./screens/home";
+
+const App = () => (
+  <Fragment>
+    <HomeScreen />
+  </Fragment>
+);
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navbar, Container } from "rbx";
+import logoImg from "../../assets/images/logo.png";
+import "../../styles/header.scss";
+
+function Header() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand>
+          <img src={logoImg} />
+          <Navbar.Burger
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbar-menu"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </Navbar.Burger>
+        </Navbar.Brand>
+
+        <Navbar.Menu id="navbar-menu">
+          <Navbar.Segment
+            as="div"
+            className="navbar-item navbar-end"
+            align="right"
+          >
+            Item 1
+          </Navbar.Segment>
+        </Navbar.Menu>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
