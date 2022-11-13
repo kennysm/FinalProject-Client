dbca5fc1289ab0fb6f185bc3b7c82e6e79fb6b83
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-11-13 19:45:18 -0300

    register form fixed

---
+++ src/components/auth/register_form/index.jsx
@@ -0,0 +1,92 @@
+import React, { Fragment, useState, useEffect } from "react";
+import {
+  Button,
+  Field,
+  Control,
+  Input,
+  Column,
+  Section,
+  Help,
+  Label,
+} from "rbx";
+import { Navigate } from "react-router-dom";
+
+function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [redirectLogin, setRedirectLogin] = useState(false);
+
+  if (redirectLogin === true) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <Fragment>
+      <Column.Group centered>
+        <form>
+          <Column size={12}>
+            <Field>
+              <Label size="small">Name:</Label>
+              <Control>
+                <Input
+                  type="name"
+                  required
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label size="small">Email:</Label>
+              <Control>
+                <Input
+                  type="email"
+                  required
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label size="small">Password:</Label>
+              <Control>
+                <Input
+                  type="password"
+                  required
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Control>
+                <Column.Group breakpoint="mobile">
+                  <Column>
+                    <a
+                      className="button is-white has-text-custom-purple"
+                      onClick={(e) => setRedirectLogin(true)}
+                    >
+                      Login or
+                    </a>
+                  </Column>
+                  <Column>
+                    <Button color="custom-purple" outlined>
+                      Register
+                    </Button>
+                  </Column>
+                </Column.Group>
+              </Control>
+            </Field>
+            {error && <Help color="danger">Email or Password invalid</Help>}
+          </Column>
+        </form>
+      </Column.Group>
+    </Fragment>
+  );
+}
+
+export default RegisterForm;
